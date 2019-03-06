cb7f3158db3b9f5eb44f2e37c3045bde59298e76
James Vanderburg <james@vanderburg.org>
2019-03-06 16:22:31 -0600

    Dynamically change header based on window size
    Added jQuery to project. Currently using it to change the header at the
    top of the page based on the window size.

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// Thinking of renaming this file... 'main' sounds bad?
+
+// var short_description = "";
+var normal_description = "Howdy! I'm James Vanderburg, a Computer Science student at Texas A&M University looking for opportunities to solve new problems frequently, gain experience with various technologies quickly, and change the world for the better for clients.";
+
+$(window).resize(function () {
+  if ($( window ).width() < 720) {
+    $(".project-tagline").html("James Vanderburg")
+  } else {
+    $(".project-tagline").html(normal_description)
+  }
+});
