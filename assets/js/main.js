74a7a8b5cf0205996189e2b0377e97a5bff75e45
James Vanderburg <james@vanderburg.org>
2019-03-06 22:12:10 -0600

    Change width checking to run on load instead of just resize

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,18 @@
 // var short_description = "";
 var normal_description = "Howdy! I'm James Vanderburg, a Computer Science student at Texas A&M University looking for opportunities to solve new problems frequently, gain experience with various technologies quickly, and change the world for the better for clients.";
 
-$(window).resize(function () {
+function checkSize() {
   if ($( window ).width() <= 720) {
     // Change this to something decently descriptive
     $(".project-tagline").html("James Vanderburg")
   } else {
     $(".project-tagline").html(normal_description)
   }
+}
+
+// run once to check on load
+checkSize();
+// do this for resizing
+$(window).resize(function () {
+  checkSize();
 });
