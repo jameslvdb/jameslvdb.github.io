a63e8d9258d547f96a47a6b4b0c4b982136a22ae
James Vanderburg <james@vanderburg.org>
2019-03-06 21:41:53 -0600

    *sigh*
    Make change < to <= for jQuery width detection

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,8 @@
 var normal_description = "Howdy! I'm James Vanderburg, a Computer Science student at Texas A&M University looking for opportunities to solve new problems frequently, gain experience with various technologies quickly, and change the world for the better for clients.";
 
 $(window).resize(function () {
-  if ($( window ).width() < 720) {
-    // Change this to something decently descriptive 
+  if ($( window ).width() <= 720) {
+    // Change this to something decently descriptive
     $(".project-tagline").html("James Vanderburg")
   } else {
     $(".project-tagline").html(normal_description)
